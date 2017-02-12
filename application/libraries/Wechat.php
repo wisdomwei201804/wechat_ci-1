53ab951d1dcc85f02ceaabe4d51ff47fc30e8ce4
hantao <1010252139@qq.com>
2017-02-12 17:29:44 +0800

    定义微信类

--- application/libraries/Wechat.php
+++ application/libraries/Wechat.php
@@ -3,3 +3,8 @@
 define("TOKEN", "weixxxxxxin");//用于微信第一次验证
 define("APPID", "wx6fxxxxxxx25ccd9");//微信公众号的APPID
 define("APPSECRET", "f17c7c6xxxxxxxxxxxxx0ab");//微信的APPSECRET
+
+class CI_Wechat {//定义微信类
+
+
+｝

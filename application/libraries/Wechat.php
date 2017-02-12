056023f21c8b2fda8255301606e7cc9c4d3f60f8
hantao <1010252139@qq.com>
2017-02-12 17:28:08 +0800

    定义微信常量

---
+++ application/libraries/Wechat.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+define("TOKEN", "weixxxxxxin");//用于微信第一次验证
+define("APPID", "wx6fxxxxxxx25ccd9");//微信公众号的APPID
+define("APPSECRET", "f17c7c6xxxxxxxxxxxxx0ab");//微信的APPSECRET

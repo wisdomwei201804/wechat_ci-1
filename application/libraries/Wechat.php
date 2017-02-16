389fa1efe334707141744a1d8908e1e65e47d6b1
hantao <1010252139@qq.com>
2017-02-16 08:31:05 +0800

    微信sdk，http请求，get方式

--- application/libraries/Wechat.php
+++ application/libraries/Wechat.php
@@ -41,4 +41,36 @@ class CI_Wechat {//定义微信类
 	     return $result;
 	 }
 
+	 /**
+	  * 构造http请求，get方式
+	  * @param string $url
+	  * @return http请求结果
+	  */
+	 private function http_get($url){
+	     $oCurl = curl_init();
+	     if(stripos($url,"https://")!==FALSE){
+	         curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	         curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+	         curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+	     }
+	     curl_setopt($oCurl, CURLOPT_URL, $url);
+	     curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+	     $sContent = curl_exec($oCurl);
+	     $aStatus = curl_getinfo($oCurl);
+	     curl_close($oCurl);
+	     if(intval($aStatus["http_code"])==200){
+	         return $sContent;
+	     }else{
+	         return false;
+	     }
+	 }
+
+
+
+
+
+
+
+
+
 ｝

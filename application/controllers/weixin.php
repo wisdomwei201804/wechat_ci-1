29391646186879c76afaa1c617d4478c001296ec
hantao <1010252139@qq.com>
2017-02-16 21:53:38 +0800

    微信后台验证代码

--- application/controllers/weixin.php
+++ application/controllers/weixin.php
@@ -15,7 +15,35 @@ class Weixin extends CI_Controller
         }
     }
 
+    //验证
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
 
+    private function checkSignature()
+    {
+
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 
